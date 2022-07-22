09d294d6bd74e5bc9ac948ba9399bf330f1480db
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-07-23 00:09:20 +0200

    vimeo player js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(settleTime, 1000));
+
+function settleTime ({seconds}) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+
+try {
+  const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+} catch (error) {
+    console.log(error.name);
+};
