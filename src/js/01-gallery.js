6bae4c0951afb3780f186b088ff144faf1f369ff
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-07-22 15:45:27 +0200

    refactoring simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+
+const gallery = document.querySelector('.gallery');
+
+const galleryList = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+).join('');
+
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+const lightbox = new SimpleLightbox('.gallery__item a', {
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
